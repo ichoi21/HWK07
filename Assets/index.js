fd9a237df8fa4a8082d0a233d335b48264120dbf
ichoi21 <ihchoi@gmail.com>
2020-05-30 18:04:11 -0700

    not done, back up

--- Assets/index.js
+++ Assets/index.js
@@ -1,12 +1,18 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
+const { app, BrowserWindow } = require("electron");
+const open = require("open");
+const promptQA = require("./promptQA");
+const htmlTemplate = require("./readmeTemp");
 
-];
+function writeToFile(fileName, data) {}
 
-function writeToFile(fileName, data) {
-}
-
-function init() {
-
-}
+function init() {}
 
 init();
+
+module.exports = {
+  promptUser,
+};
