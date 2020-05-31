3e9a82dee823cf717a45a68fd1c0af954673a6f6
ichoi21 <ihchoi@gmail.com>
2020-05-31 14:07:58 -0700

    redo... nothing works

--- Assets/index.js
+++ Assets/index.js
@@ -1,41 +1,85 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
-const promptQA = require("./promptQA");
-const genReadme = require("./readmeTemp");
+// const promptQA = require("./promptQA");
+// const genReadme = require("./readmeTemp");
 
-// function writeToFile(fileName, data) {}
+inquirer
+  .prompt(
+    {
+      type: "input",
+      name: "userName",
+      message: "What is your GitHub USERNAME?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your GitHub EMAIL",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the Project's TITLE?",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Can you describe what this project is ABOUT?",
+    },
+    {
+      type: "input",
+      name: "install",
+      message: "How is this project installed?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "What is the **INTENT** use of this project?",
+    },
+    {
+      type: "list",
+      nane: "license",
+      message: "Which License are you using?",
+      choices: [
+        "GNU GPLv3",
+        "Mozilla Public License 2.0",
+        "Apache 2.0",
+        "MIT License",
+        "Boost Software License 1.0",
+        "None",
+      ],
+    }
+    // {
+    //   type: "confirm",
+    //   name: "contributor",
+    //   message: "Did any other else work on this Project?",
+    // },
+    // {
+    //   type: "input",
+    //   message: "Enter NAMES of all Contributors:",
+    //   name: "contributorRes",
+    //   when: (answers) => answers.contributor === true,
+    // }
+  )
+  .then(function ({ userName }) {
+    const searchUrl = `https://api.github.com/users/${userName}`;
 
-// function init() {}
+    axios.get(searchUrl).then(function (res) {
+      var avatarUrl = res.avatar_url;
 
-// init();
-
-// module.exports = {
-//   promptUser,
-// };
-
-inquirer.prompt(promptQA).then(function ({ username }) {
-  const searchUrl = `https://api.github.com/users/${username}`;
-
-  axios.get(searchUrl).then(function (response) {
-    var avatarUrl = response.avatar_url;
-    var userName = response.email;
-    var userlink = response.html_url;
-
-    inquirer.prompt(promptQA).then(function (res) {
-      if (res.contributorRes === "undefined") {
-        res.contributorRes = "None";
-      }
       console.log(
+        res.avatar_url,
         res.title,
         res.about,
+        avatarUrl,
+        userName,
+        res.html_link,
+        email,
         res.install,
         res.usage,
         res.contributor,
         res.contributorRes,
         res.license
       );
-      fs.writeFile(genReadme);
     });
   });
-});
