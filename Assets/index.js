17d2ef958f8839aa11e2ce6363055b85740675e0
ichoi21 <ihchoi@gmail.com>
2020-05-31 14:39:08 -0700

    promptQA.js is imported

--- Assets/index.js
+++ Assets/index.js
@@ -1,85 +1,28 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
-// const promptQA = require("./promptQA");
+const promptQA = require("./promptQA");
 // const genReadme = require("./readmeTemp");
 
-inquirer
-  .prompt(
-    {
-      type: "input",
-      name: "userName",
-      message: "What is your GitHub USERNAME?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is your GitHub EMAIL",
-    },
-    {
-      type: "input",
-      name: "title",
-      message: "What is the Project's TITLE?",
-    },
-    {
-      type: "input",
-      name: "about",
-      message: "Can you describe what this project is ABOUT?",
-    },
-    {
-      type: "input",
-      name: "install",
-      message: "How is this project installed?",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "What is the **INTENT** use of this project?",
-    },
-    {
-      type: "list",
-      nane: "license",
-      message: "Which License are you using?",
-      choices: [
-        "GNU GPLv3",
-        "Mozilla Public License 2.0",
-        "Apache 2.0",
-        "MIT License",
-        "Boost Software License 1.0",
-        "None",
-      ],
-    }
-    // {
-    //   type: "confirm",
-    //   name: "contributor",
-    //   message: "Did any other else work on this Project?",
-    // },
-    // {
-    //   type: "input",
-    //   message: "Enter NAMES of all Contributors:",
-    //   name: "contributorRes",
-    //   when: (answers) => answers.contributor === true,
-    // }
-  )
-  .then(function ({ userName }) {
-    const searchUrl = `https://api.github.com/users/${userName}`;
+inquirer.prompt(promptQA).then(function ({ userName }) {
+  const searchUrl = `https://api.github.com/users/${userName}`;
 
-    axios.get(searchUrl).then(function (res) {
-      var avatarUrl = res.avatar_url;
+  axios.get(searchUrl).then(function (res) {
+    var avatarUrl = res.data.avatar_url;
+    var Url = res.data.avatar_url;
 
-      console.log(
-        res.avatar_url,
-        res.title,
-        res.about,
-        avatarUrl,
-        userName,
-        res.html_link,
-        email,
-        res.install,
-        res.usage,
-        res.contributor,
-        res.contributorRes,
-        res.license
-      );
-    });
+    console.log(
+      res.title,
+      res.about,
+      avatarUrl,
+      userName,
+      res.data.html_link,
+      // email,
+      res.install,
+      res.usage
+      // res.contributor,
+      // res.contributorRes,
+      // license
+    );
   });
+});
