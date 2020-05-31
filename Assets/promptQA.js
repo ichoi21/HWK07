c5f60d2fc1bb9cc9379695aec49f1a6cfaaabeb3
ichoi21 <ihchoi@gmail.com>
2020-05-30 18:03:04 -0700

    Qs for Prompt

---
+++ Assets/promptQA.js
@@ -0,0 +1,18 @@
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your GitHub email?",
+  },
+  {
+    type: "list",
+    name: "lang",
+    choices: ["HTML", "CSS", "Javascript", "APIs + AJAX", "jQuery", "Node"],
+    message: "What have you enjoyed so far in class?",
+  },
+];
