7fb0e1d74a4777b146cad45d7d14fe29b65471a3
ichoi21 <ihchoi@gmail.com>
2020-05-30 18:49:23 -0700

    matched questions with requirements

--- Assets/promptQA.js
+++ Assets/promptQA.js
@@ -1,18 +1,57 @@
 const questions = [
   {
     type: "input",
-    name: "github",
-    message: "What is your GitHub username?",
+    name: "username",
+    message: "What is your GitHub USERNAME?",
   },
   {
     type: "input",
     name: "email",
-    message: "What is your GitHub email?",
+    message: "What is your GitHub EMAIL",
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is the Project's TITLE?",
+  },
+  {
+    type: "input",
+    name: "about"
+    message: "Can you describe what this project is ABOUT?",
+    name: "desc",
+  },
+  {
+    type: "input",
+    name: "install",
+    message: "How is this project installed?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the **INTENT** use of this project?",
   },
   {
     type: "list",
-    name: "lang",
-    choices: ["HTML", "CSS", "Javascript", "APIs + AJAX", "jQuery", "Node"],
-    message: "What have you enjoyed so far in class?",
+    nane: "license",
+    message: "Which License are you using?",
+    choices: [
+      "GNU GPLv3",
+      "Mozilla Public License 2.0",
+      "Apache 2.0",
+      "MIT License",
+      "Boost Software License 1.0",
+      "None",
+    ],
+  },
+  {
+    type: "confirm",
+    name: "contributor",
+    message: "Did any other else work on this Project?",
+  },
+  {
+    type: "input",
+    message: "Enter NAMES of all Contributors:",
+    name: "contributorRes",
+    when: (answers) => answers.contributor === true,
   },
 ];
